434e8d1ccde8ca5e82c71825095030c2cc401cf5
amit verma <amitv.aa10@gmail.com>
2023-08-18 15:50:26 +0530

    added db.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,16 @@ import React, { useEffect, useState } from "react";
 
 function Home(){
 const [data,setData] = useState([])
+{
+    useEffect(()=>{
+        axios.get('http://localhost:3035/users').then
 
+    },[])
+    return (
+        <div>
+            <p>home page</p>
+        </div>
+    )
+};
 };
 export default Home;
