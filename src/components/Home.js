4f64b59101ac70b3bc5cfb0a16995ad9673ed72d
amit verma <amitv.aa10@gmail.com>
2023-08-18 18:49:33 +0530

    added axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,21 @@ import React, { useEffect, useState } from "react";
 
 function Home(){
 const [data,setData] = useState([])
-{
+
     useEffect(()=>{
         axios.get('http://localhost:3035/users').then
+        (res =>setData(res.data))
+        .catch(error =>console.log(error))
 
-    },[])
+    },[]);
+    
+    console.log("&&&&data",data);
     return (
+        
         <div>
             <p>home page</p>
         </div>
     )
 };
-};
+
 export default Home;
