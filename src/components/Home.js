d28619ccab0ca49109ea7745e9f30dcd9c0856c6
amit verma <amitv.aa10@gmail.com>
2023-08-21 19:15:32 +0530

    displaying data from json-server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,56 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+// import './home.scss';
+import './home.scss';
+import InputForm from "./form/InputForm";
 
 function Home(){
-const [data,setData] = useState([])
+const [data,setData] = useState([]);
+const [showForm,setShowForm] = useState(false);
 
-    useEffect(()=>{
+    useEffect(()=>{  
         axios.get('http://localhost:3035/users').then
         (res =>setData(res.data))
         .catch(error =>console.log(error))
 
     },[]);
     
+    const handleClick=() =>{
+        setShowForm(true);
+    }
     console.log("&&&&data",data);
     return (
-        
         <div>
-            <p>home page</p>
+        <div className="heading">home page</div> 
+        <div className="home">   
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item,index)=>{
+                            return <tr key ={index}>
+                             <td>{item.id}</td>
+                             <td>{item.name}</td>
+                             <td>{item.email}</td>
+                             <td>{item.age}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+
+        <button onClick ={handleClick}>Form</button>
+        {showForm &&
+        <InputForm/>
+        }
         </div>
     )
 };
---
+++ src/components/form/InputForm.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+
+function InputForm() {
+
+    return (
+        <div>
+            <p>hhhhh</p>
+       
+        </div>
+    )
+};
+
+export default InputForm;
